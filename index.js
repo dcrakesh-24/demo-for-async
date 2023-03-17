ef5674c43104d89d954a2644049592bce3a11970
dcrakesh-24 <rakeshmudhaliyar@gmail.com>
2023-03-17 15:44:01 +0530

    rakki

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log('rakki')
+// for (let i=0; i<1000; i++){
+//     console.log("loop is running..");
+// }
+// console.log('charan');
+
+// console.log('the end');
+
+console.log("first")
+
+setTimeout(() => {
+    console.log("timeout function")
+    
+}, 5000);
+
+console.log("end")
